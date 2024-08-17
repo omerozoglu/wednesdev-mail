6a9ed226204fb4b153bac3f341048f5f833ad5a1
omerozoglu <omerozoglu.1@gmail.com>
2024-08-18 02:31:45 +0300

    feat: Create email_attachments table migration

---
+++ database/migrations/2024_08_17_231233_create_email_attachments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('email_attachments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('email_id')->constrained('emails');
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('email_attachments');
+    }
+};
