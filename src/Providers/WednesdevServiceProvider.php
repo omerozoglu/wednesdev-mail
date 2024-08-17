dbee83c2d509ae17c0cdb2aea16f2598eec9667d
omerozoglu <omerozoglu.1@gmail.com>
2024-08-17 18:17:54 +0300

    refactor: Package Structure.

---
+++ src/Providers/WednesdevServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace YourVendor\YourPackageName\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class YourPackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
