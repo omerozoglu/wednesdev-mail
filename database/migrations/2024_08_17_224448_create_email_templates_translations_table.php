9f9680dc794ddedb39a5b5609f3004f41fb5b9b8
omerozoglu <omerozoglu.1@gmail.com>
2024-08-18 02:31:22 +0300

    feat: Create email_templates_translations table migration

---
+++ database/migrations/2024_08_17_224448_create_email_templates_translations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('email_templates_translations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('email_template_id')->constrained('email_templates');
+            $table->string('locale')->index();
+            $table->string('subject')->nullable();
+            $table->text('body')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('email_templates_translations');
+    }
+};
