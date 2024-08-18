c269184a3e2328ab33c4c98d7dc6d115853ae1e0
omerozoglu <omerozoglu.1@gmail.com>
2024-08-18 05:40:07 +0300

    fix: Fix Mail service provider.

--- src/Providers/MailServiceProvider.php
+++ src/Providers/MailServiceProvider.php
@@ -3,14 +3,44 @@
 namespace Wednesdev\Mail\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Wednesdev\Mail\Commands\EmailPackageCommand;
+use Wednesdev\Mail\Services\EmailService;
 
 class MailServiceProvider extends ServiceProvider
 {
-    public function register()
+    /**
+     * @return void
+     */
+    public function register(): void
     {
+        $this->app->singleton('email', function ($app) {
+            return new EmailService($app['mailer']);
+        });
     }
 
-    public function boot()
+    /**
+     * @return void
+     */
+    public function boot(): void
     {
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // Publish migrations
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
+
+        // Publish seeder
+        $this->publishes([
+            __DIR__.'/../database/seeders' => database_path('seeders'),
+        ], 'seeders');
+
+        // Register the seeder
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                EmailPackageCommand::class,
+            ]);
+        }
     }
 }
