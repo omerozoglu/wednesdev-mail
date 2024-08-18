de8d35e4bdf56a2ecbecec5f144535445cd358f4
omerozoglu <omerozoglu.1@gmail.com>
2024-08-18 05:37:53 +0300

    feat: Create Email facade

--- src/Facades/Email.php
+++ src/Facades/Email.php
@@ -4,6 +4,10 @@ namespace Wednesdev\Mail\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static queue($mailable, string $templateName, string $to, array $data = [], string $locale = null)
+ * @method static send($mailable, string $templateName, string $to, array $data = [], string $locale = null)
+ */
 class Email extends Facade
 {
     protected static function getFacadeAccessor(): string
